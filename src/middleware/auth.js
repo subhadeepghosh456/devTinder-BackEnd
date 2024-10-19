a2219fd2d0de9e2e33c800f69b5dcf088b95c263
subhadeep <srsubhadeepghosh456@gmail.com>
2024-10-19 22:10:04 +0530

    changes in auth middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,7 +5,7 @@ const userAuth = async (req, res, next) => {
     const { token } = req.cookies;
 
     if (!token) {
-      throw new Error("Invalid Token");
+      return res.status(401).send("Please Login!!!");
     }
 
     const decodedMessage = await jwt.verify(token, "DEV@Tinder$790");
