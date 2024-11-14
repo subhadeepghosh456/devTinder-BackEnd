cf06fdf88afcc72c883bfc849284b6df3364c86f
subhadeep <srsubhadeepghosh456@gmail.com>
2024-11-14 23:22:14 +0530

    some changes

--- src/app.js
+++ src/app.js
@@ -13,12 +13,14 @@ const userRouter = require("./routes/user");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
+
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
